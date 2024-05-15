67b97118faf44f43332769f4ff411e121f28d697
seetvn <waiyansteven2002@gmail.com>
2024-05-15 17:58:17 +0100

    added priority

--- frontend/src/Todo.tsx
+++ frontend/src/Todo.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
 
+// type Todo struct {
+//   ID   int    `json:"id"`
+//   Task string `json:"task"`
+// Description string `json:"description"`
+// Priority string `json:"priority"`
+// Finished bool `json:"finished"`
+// }
+
 export type TodoType = {
   task: string,
   description: string,
+  priority: string,
+
 }
 
-function Todo({ task, description }: TodoType) {
+function Todo({ task, description, priority }: TodoType) {
   return (
     <div className="todo">
       <div className="todo-details">
         <p className="todo-task">{task}</p>
         <p className="todo-description">{description}</p>
+        <p className="todo-priority">Priority : {priority}</p>
       </div>
     </div>
   );
